4566e24a834c5a239aac61c9ef81a4a49fa54737
Greih Murray <murrayg0216@outlook.com>
2022-09-07 16:38:40 -0500

    Adding central run method
    Added a single method which will be used to compute scores for both the
    from scratch method and the anything goes method
    
    Also made minor changes to the from_scratch implementation so a user can
    specify if hyper parameter tuning should take place of not. If not, uses
    hard coded parameters based on optimal performance found with
    hyperparameter tuning

--- from_scratch.py
+++ from_scratch.py
@@ -1,263 +1,357 @@
-import utility
+# Load Data
+# Clean data
+# Get priors
+
+import csv
 import math
 
+def read_file(filename):
+    positive = []
+    negative = []
 
-# Global vars for hyperparameter tuning
-alphas = [1] #[5, 4, 3, 2, 1, 0.9, 0.3, 0.2, 0.1]
+    # Loading data from file
+    with open(filename, encoding="utf-8") as file:
+        f = csv.reader(file, delimiter="\t")
+        all_data = []
 
-min_counts = [0, 1, 2, 3, 4, 5, 6]
+        for line in f:
+            if len(line) >= 2:
+                all_data.append(line)
 
-max_iters = [0]
+        # Splitting data into train and test before anything else
+        train, test = train_test_split(all_data)
 
-lil_test = [['0', '@USER amseru gwych ar gyfer y Pasg']]
+        # Dividing positive and negative sentiments
+        for line in train:
+            if(len(line) >= 2):
+                if line[0] == '1':
+                    positive.append(line[1])
+                if line[0] == '0':
+                    negative.append(line[1])
 
+    # Calculate base percentage of each class
+    offsets = [(len(positive) / (len(positive) + len(negative))), (len(negative) / (len(positive) + len(negative)))]
 
-# Runs majority of program
-def main():
-    max_acc = {
-        'score': 0,
-        'min_count': 0,
-        'max_iter': 0,
-        'alpha': 0
-    }
-    max_pre = {
-        'score': 0,
-        'min_count': 0,
-        'max_iter': 0,
-        'alpha': 0
-    }
-    max_rec = {
-        'score': 0,
-        'min_count': 0,
-        'max_iter': 0,
-        'alpha': 0
-    }
-    max_f = {
-        'score': 0,
-        'min_count': 0,
-        'max_iter': 0,
-        'alpha': 0
-    }
-
-    # Call read_file
-    print("Loading data...")
-    positive, negative, test, offsets = utility.read_file("train.tsv")
-
-    # Set offsets for easier access
-    pos_offset = -1 * math.log(offsets[0])
-    neg_offset = -1 * math.log(offsets[1])
-
-    # pos_priors, neg_priors = utility.get_priors(positive, negative)
-    #
-    # predict(lil_test, [pos_offset, neg_offset], pos_priors, neg_priors, 0.1)
-
-    # Run for every min count in list
-    for min_count in min_counts:
-        # Run for every max_iter in list
-        for max_iter in max_iters:
-            # Tuning for alphas
-            print("\n\nCalculating priors for min count of ", min_count, " and max iterations of ", max_iter, "...",
-                  sep="")
-            for alpha in alphas:
-                # Calculate priors
-                positive_priors, negative_priors = utility.get_priors(positive, negative, alpha, min_count)
-
-                # Make predictions
-                print("\nPredicting...")
-                scores = predict(test, [pos_offset, neg_offset], positive_priors, negative_priors, alpha)
-
-                if scores[0] > max_acc['score']:
-                    max_acc['score'] = scores[0]
-                    max_acc['min_count'] = min_count
-                    max_acc['max_iter'] = max_iter
-                    max_acc['alpha'] = alpha
-                if scores[1] > max_pre['score']:
-                    max_pre['score'] = scores[1]
-                    max_pre['min_count'] = min_count
-                    max_pre['max_iter'] = max_iter
-                    max_pre['alpha'] = alpha
-                if scores[2] > max_rec['score']:
-                    max_rec['score'] = scores[2]
-                    max_rec['min_count'] = min_count
-                    max_rec['max_iter'] = max_iter
-                    max_rec['alpha'] = alpha
-                if scores[3] > max_f['score']:
-                    max_f['score'] = scores[3]
-                    max_f['min_count'] = min_count
-                    max_f['max_iter'] = max_iter
-                    max_f['alpha'] = alpha
+    return positive, negative, test, offsets
 
 
-    # Helper function
-    test_priors(positive_priors, negative_priors)
+def train_test_split(data, percent = 0.2):
+    percent = int(percent * 10)
 
-    print("\n\nMAXIMUM SCORES AND PARAMETERS\n")
-    print("ACCURACY: ", max_acc['score'], " MIN COUNT: ", max_acc['min_count'], ' MAX ITERS: ',
-          max_acc['max_iter'], ' Alpha: ', max_acc['alpha'])
-    print("PRECISION: ", max_pre['score'], " MIN COUNT: ", max_pre['min_count'], ' MAX ITERS: ',
-          max_pre['max_iter'], ' Alpha: ', max_pre['alpha'])
-    print("RECALL: ", max_rec['score'], " MIN COUNT: ", max_rec['min_count'], ' MAX ITERS: ',
-          max_rec['max_iter'], ' Alpha: ', max_rec['alpha'])
-    print("F SCORE: ", max_f['score'], " MIN COUNT: ", max_f['min_count'], ' MAX ITERS: ',
-          max_f['max_iter'], ' Alpha: ', max_f['alpha'])
+    train = data[((len(data) // 10) * percent):]
+    test = data[:((len(data) // 10) * percent)]
+
+    return train, test
+
+def clean_data(data):
+    chars_to_remove = '!@#$%^&*()-_`~=+;:,./<>?\'\"\\|'
+    clean_data = []
+
+    for line in data:
+        line = ''.join([char for char in line if char not in chars_to_remove])
+        clean_data.append(line.lower())
+
+    return clean_data
 
 
-# Helper function to better understand how many words are more positive than negative
-def test_priors(pos, neg):
-    total_pos = 0
-    total_neg = 0
+def get_counts(pos, neg):
+    counts = {}
 
-    for key in pos.keys():
-        # If word is more positive, increment total_pos, else increment total_neg
-        if key in neg.keys():
-            if pos[key] > neg[key]:
-                #print("Pos: ", pos[key], "   Neg: ", neg[key], "       KEY: ", key, "     POSITIVE")
-                total_pos += 1
-            if pos[key] < neg[key]:
-                #print("Pos: ", pos[key], "   Neg: ", neg[key], "       KEY: ", key, "     NEGATIVE")
-                total_neg += 1
+    for ngram in pos:
+        if ngram in counts.keys():
+            counts[ngram] = {'pos': (counts[ngram]['pos']+1), 'neg': counts[ngram]['neg']}
         else:
-            total_pos += 1
+            counts[ngram] = {'pos': 1, 'neg': 0}
 
-    for key in neg.keys():
-        if key not in pos.keys():
-            total_neg += 1
+    for ngram in neg:
+            if ngram in counts.keys():
+                counts[ngram] = {'pos': counts[ngram]['pos'], 'neg': (counts[ngram]['neg'] + 1)}
+            else:
+                counts[ngram] = {'pos': 0, 'neg': 1}
 
-    # Print total words that are more positive or negative
-    print("\n\nTotal Positive: ", total_pos, "        Total Negative: ", total_neg)
+    return counts
 
 
-# Make predictions based on priors
-def predict(test_data, offsets, positive_priors, negative_priors, alpha):
+def get_priors(counts, pos, neg, alpha):
+    total_neg_words = 0
+    total_pos_words = 0
+    uniques = len(counts.keys())
+    priors = {}
+
+    for key in counts.keys():
+        total_pos_words += counts[key]['pos']
+        total_neg_words += counts[key]['neg']
+
+    for key in counts.keys():
+        pos_prior = (counts[key]['pos'] + alpha)/(total_pos_words + (uniques * alpha))
+        neg_prior = (counts[key]['neg'] + alpha)/(total_neg_words + (uniques * alpha))
+
+        priors[key] = {'pos': math.log(pos_prior), 'neg':math.log(neg_prior)}
+
+    return priors
+
+
+def split_data(data):
+    sentiment = []
+    tweet = []
+
+    for entry in data:
+        sentiment.append(entry[0])
+        tweet.append(entry[1])
+
+    return sentiment, tweet
+
+def predict(test_data, priors, offsets, n):
+    sentiments, data = split_data(test_data)
+
+    data = clean_data(data)
+    data = not_hack(data)
 
     all_predicts = []
-    total_neg = 0
-    total = 0
-    spec_chars = '!@#$%?.,\"\'`~;:{}[]()-'
 
-    # run for each test tweet
-    for text in test_data:
+    for line in data:
+        predict = -1
         pos_sum = offsets[0]
         neg_sum = offsets[1]
+        line = line.split(" ")
 
-        predict = -1
+        cur_grams = [line[i:i + n] for i in range(len(line) - n + 1)]
 
-        tripper = 0
-        iter = 0
+        for gram in cur_grams:
+            gram = ''.join(gram)
+            if gram in priors.keys():
+                pos_sum += priors[gram]['pos']
+                neg_sum += priors[gram]['neg']
 
-        # Split tweet into words
-        for word in text[1].split(" "):
-            word = word.translate({ord(i): None for i in spec_chars})
-
-            word = word.lower()
-
-            # Calculate the probability of each class
-            if word in positive_priors.keys():
-                pos_sum += positive_priors[word]
-            if word in negative_priors.keys():
-                neg_sum += negative_priors[word]
-
-        #print(neg_sum, "    |    ", pos_sum)
-
-        # If neg_sum is greater, predict negative sentiment and vice versa
-        if neg_sum > pos_sum:
-            predict = 0
-            total_neg += 1
         if pos_sum > neg_sum:
             predict = 1
+        else:
+            predict = 0
 
-        #print(predict,"***", text[0])
+        all_predicts.append(predict)
 
-        # Append to predictions in form [PREDICTION, ACTUAL]
-        total += 1
-        all_predicts.append([str(predict), text[0]])
+    acc = calc_acc(all_predicts, sentiments)
+    precision = calc_precision(all_predicts, sentiments)
+    recall = calc_recall(all_predicts, sentiments)
+    fscore = calc_fscore(all_predicts, sentiments)
 
-    # Print total negative predictions vs total predictions (Just for reference)
-    print(total_neg, total)
-
-    print("Calculating scores for Alpha ", alpha, "...", sep="")
-
-    scores = [accuracy(all_predicts), precision(all_predicts), recall(all_predicts),fscore(all_predicts)]
-
-    # Call functions to calculate scores and print
-    print("\nScores for Alpha ", alpha, ":", sep="")
-    print(scores[0], "% Accuracy", sep="")
-    print(scores[1], "% Precision", sep="")
-    print(scores[2], "% Recall", sep="")
-    print(scores[3], "% F Score", sep="")
+    scores = [acc, precision, recall, fscore]
 
     return scores
 
-# Calculate the accuracy
-def accuracy(all_predicts):
-    total_right = 0
-    total = 0
 
-    # For each prediction that is right, increment
-    for predict in all_predicts:
-        if predict[0] == predict[1]:
+def calc_acc(predicts, sentiments):
+    total_right = 0
+
+    for i in range(0, len(predicts)):
+        if str(predicts[i]) == str(sentiments[i]):
             total_right += 1
 
-        total += 1
-
-    # Calculate accuracy
-    acc = 100 * (total_right/total)
+    acc = 100 * (total_right / len(predicts))
 
     return acc
 
 
-# Calculate precision
-def precision(all_predicts):
+def calc_precision(predicts, sentiments):
     true_pos = 0
     false_pos = 0
 
-    # Tally true and false positives
-    for predict in all_predicts:
-        # True positives
-        if predict[0] == '1' and predict[1] == '1':
+    for i in range(0, len(predicts)):
+        if str(predicts[i]) == '1' and str(sentiments[i]) == '1':
             true_pos += 1
-        # False positives
-        if predict[0] == '1' and predict[1] != '1':
+        if str(predicts[i]) == '1' and str(sentiments[i]) != '1':
             false_pos += 1
 
-    # Calculate precision
-    precis = 100 * (true_pos / (true_pos + false_pos))
+    if (true_pos + false_pos) == 0:
+        return 0.01
 
-    return precis
+    precision = 100 * (true_pos / (true_pos + false_pos))
 
-# Calculate recall
-def recall(all_predicts):
+    return precision
+
+
+def calc_recall(predicts, sentiments):
     true_pos = 0
     false_neg = 0
 
-    # Find true positives and false negatives
-    for predict in all_predicts:
-        # True positive
-        if predict[0] == '1' and predict[1] == '1':
+    for i in range(0, len(predicts)):
+        if str(predicts[i]) == '1' and str(sentiments[i]) == '1':
             true_pos += 1
-        # False negative
-        if predict[0] == '0' and predict[1] == '1':
+        if str(predicts[i]) == '0' and str(sentiments[i]) == '1':
             false_neg += 1
 
-    # Calcualte recall
-    reca = 100 * (true_pos / (true_pos + false_neg))
+    recall = 100 * (true_pos / (true_pos + false_neg))
 
-    return reca
+    return recall
 
-# Calculate fscore
-def fscore(all_predicts):
-    # Get precision and recall
-    rec = recall(all_predicts)
-    prec = precision(all_predicts)
 
-    # If precision + recall is not 0, calculate f score, otherwise, default to 0
-    if prec + rec != 0:
-        f_score = ((2 * prec * rec)/(prec + rec))
+def calc_fscore(predicts, sentiments):
+    recall = calc_recall(predicts, sentiments)
+    precision = calc_precision(predicts, sentiments)
+    fscore = 0
+
+    if recall + precision != 0:
+        fscore = ((2 * precision * recall) / (precision + recall))
+
+    return fscore
+
+
+def print_scores(scores):
+    print("ACCURACY: ", scores[0], sep="")
+    print("PRECISION: ", scores[1], sep="")
+    print("RECALL: ", scores[2], sep="")
+    print("F SCORE: ", scores[3], sep="")
+
+
+def min_count_remove(data, min_count):
+    for key in data.keys():
+        if data[key]['pos'] <= min_count:
+            data[key]['pos'] = 0
+        if data[key]['neg'] <= min_count:
+            data[key]['neg'] = 0
+
+    return data
+
+
+def ngrams_gen(data, n):
+    ngrams = []
+
+    if n > 0:
+        for line in data:
+            line = line.split(" ")
+
+            cur_grams = [line[i:i+n] for i in range(len(line)-n+1)]
+
+            for gram in cur_grams:
+                ngrams.append(' '.join(gram))
+
+    return ngrams
+
+
+def not_hack(data):
+    new_data = []
+
+    for line in data:
+        line = line.split(" ")
+        tripper = 0
+        cur_line = []
+
+        for word in line:
+            if word == 'not' or word == 'nodyn':
+                tripper = 1
+                continue
+
+            if tripper == 1:
+                word = "NOT" + word
+
+            cur_line.append(word)
+
+        new_data.append(' '.join(cur_line))
+
+    return new_data
+
+
+def run_from_scratch(hyper_tune = False):
+    max_acc = {
+        'score': 0,
+        'min_count': 0,
+        'alpha': 0,
+        'ngram': 0
+    }
+    max_pre = {
+        'score': 0,
+        'min_count': 0,
+        'alpha': 0,
+        'ngram': 0
+    }
+    max_rec = {
+        'score': 0,
+        'min_count': 0,
+        'alpha': 0,
+        'ngram': 0
+    }
+    max_f = {
+        'score': 0,
+        'min_count': 0,
+        'alpha': 0,
+        'ngram': 0
+    }
+
+    best_params = {
+        'alpha': 1,
+        'ngram': 1,
+        'min_count': 1
+    }
+
+    alphas = [1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0.09, 0.08, 0.07, 0.06, 0.05]
+    min_counts = [0, 1, 2, 3, 4, 5]
+    ngrams = [1, 2, 3, 4]
+
+    pos, neg, test, offsets = read_file('train.tsv')
+
+    pos = clean_data(pos)
+    neg = clean_data(neg)
+
+    pos = not_hack(pos)
+    neg = not_hack(neg)
+
+    if hyper_tune is True:
+        for ngram in ngrams:
+            print("Ngrams of size: ", ngram, "...", sep="")
+
+            pos_ngrams = ngrams_gen(pos, ngram)
+            neg_ngrams = ngrams_gen(neg, ngram)
+
+            counts_full = get_counts(pos_ngrams, neg_ngrams)
+
+            for min_count in min_counts:
+                counts = min_count_remove(counts_full, min_count)
+                print("Min count of ", min_count, "...", sep="")
+                for alpha in alphas:
+                    print("\n\nCalculating priors and predicting for alpha ", alpha, "...\n", sep="")
+                    priors = get_priors(counts, pos, neg, alpha)
+
+                    scores = predict(test, priors, offsets, ngram)
+
+                    print_scores(scores)
+
+                    if scores[0] > max_acc['score']:
+                        max_acc['score'] = scores[0]
+                        max_acc['min_count'] = min_count
+                        max_acc['alpha'] = alpha
+                        max_acc['ngram'] = ngram
+                    if scores[1] > max_pre['score']:
+                        max_pre['score'] = scores[1]
+                        max_pre['min_count'] = min_count
+                        max_pre['alpha'] = alpha
+                        max_pre['ngram'] = ngram
+                    if scores[2] > max_rec['score']:
+                        max_rec['score'] = scores[2]
+                        max_rec['min_count'] = min_count
+                        max_rec['alpha'] = alpha
+                        max_rec['ngram'] = ngram
+                    if scores[3] > max_f['score']:
+                        max_f['score'] = scores[3]
+                        max_f['min_count'] = min_count
+                        max_f['alpha'] = alpha
+                        max_f['ngram'] = ngram
+
+        print("\n\nMAXIMUM SCORES AND PARAMETERS\n")
+        print("ACCURACY: ", max_acc['score'], " MIN COUNT: ", max_acc['min_count'], ' Alpha: ', max_acc['alpha'], ' NGrams: ', max_acc['ngram'])
+        print("PRECISION: ", max_pre['score'], " MIN COUNT: ", max_pre['min_count'], ' Alpha: ', max_pre['alpha'], ' NGrams: ', max_pre['ngram'])
+        print("RECALL: ", max_rec['score'], " MIN COUNT: ", max_rec['min_count'], ' Alpha: ', max_rec['alpha'], ' NGrams: ', max_rec['ngram'])
+        print("F SCORE: ", max_f['score'], " MIN COUNT: ", max_f['min_count'], ' Alpha: ', max_f['alpha'], ' NGrams: ', max_f['ngram'])
+
     else:
-        f_score = 0
+        pos_ngrams = ngrams_gen(pos, best_params['ngram'])
+        neg_ngrams = ngrams_gen(neg, best_params['ngram'])
 
-    return f_score
+        counts_full = get_counts(pos_ngrams, neg_ngrams)
 
+        counts = min_count_remove(counts_full, best_params['min_count'])
 
-if __name__ == "__main__":
-    main()
+        priors = get_priors(counts, pos, neg, best_params['alpha'])
+
+        scores = predict(test, priors, offsets, best_params['ngram'])
+
+        print_scores(scores)
---
+++ main_run.py
@@ -0,0 +1,9 @@
+import from_scratch
+
+
+def main():
+    from_scratch.run_from_scratch()
+
+
+if __name__ == "__main__":
+    main()
