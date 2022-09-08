7ae5a0c590b92184271029c33da6a5b8c468b565
Greih Murray <murrayg0216@outlook.com>
2022-09-08 15:46:35 -0500

    Finalizing project
    Finalizing the main_run file making use of an edited version of Uday's
    code with his model for the anything goes implementation

--- main_run.py
+++ main_run.py
@@ -1,9 +1,11 @@
+import anything_goes
 import from_scratch
 
 
 def main():
     from_scratch.run_from_scratch(with_test=True)
 
+    anything_goes.run_anything_goes()
 
 if __name__ == "__main__":
     main()
